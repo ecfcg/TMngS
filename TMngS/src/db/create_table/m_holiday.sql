ac5be72e134195a9a6c7e37b5a9978f16a94747e
nabe <e.cfcg9dagego9h9edeofadafeeoed@gmail.com>
2020-10-27 23:25:31 +0900

    コメントを追加

--- TMngS/src/db/create_table/m_holiday.sql
+++ TMngS/src/db/create_table/m_holiday.sql
@@ -1,3 +1,6 @@
+drop table if exists tmngs.m_holiday cascade;
 create table tmngs.m_holiday (
      holiday date primary key
 );
+comment on table tmngs.m_holiday is 'システム休日管理';
+comment on column tmngs.m_holiday.holiday is '休日';
